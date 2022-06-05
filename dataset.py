6c8993ff9092bd3792e108b00d23f822897a0355
wangzhesun <wangzhe.sun@outlook.com>
2022-06-05 15:58:40 -0500

    add dictionary storage for dataset

--- dataset.py
+++ dataset.py
@@ -11,14 +11,20 @@ class Dataset(data.Dataset):
         self.transform_ = transform
         self.train_ = np.array(glob.glob(train_path + '/*'))
         self.target_ = np.array(glob.glob(target_path + '/*'))
+        self.train_data = {}
+        self.target_data = {}
         assert len(self.train_) == len(self.target_)
 
     def __getitem__(self, index):
+        if index in self.train_data:
+            return self.train_data[index], self.target_data[index]
         train_index_img = cv.imread(self.train_[index])
         target_index_img = cv.imread(self.target_[index])
         if self.transform_:
             train_index_img = self.transform_(train_index_img)
             target_index_img = self.transform_(target_index_img)
+        self.train_data[index] = train_index_img
+        self.target_data[index] = target_index_img
         return train_index_img, target_index_img
 
     def __len__(self):
@@ -49,3 +55,7 @@ if __name__ == '__main__':
 
     a = Dataset(train_path, target_path, transform=transforms.ToTensor())
     a.display(label='train', index=2)
+    a.display(label='train', index=2)
+    a.display(label='train', index=2)
+    a.display(label='train', index=3)
+    a.display(label='train', index=3)

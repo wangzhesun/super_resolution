1b9f78e32cba27905ecfed4c09c7ceb72f06ddc9
wangzhesun <wangzhe.sun@outlook.com>
2022-06-07 20:49:22 -0500

    Update train.py

---
+++ train.py
@@ -0,0 +1,22 @@
+from model import edsr
+import dataset
+import torchvision.transforms as transforms
+import trainer
+
+
+if __name__=='__main__':
+    # load dataset
+    print('loading dataset ...')
+    train_data = dataset.Dataset(train_root='./data/DIV2K_aug/DIV2K_train_LR_bicubic_X4_aug',
+                                 target_root='./data/DIV2K_aug/DIV2K_train_HR_X4_aug',
+                                 transform=transforms.ToTensor())
+
+    # initialize model
+    print('building model ...')
+    EDSR = edsr.EDSR(scale=4)
+
+    # train the model
+    trainer = trainer.Trainer(train_data, EDSR)
+    trainer.set_checkpoint_saving_path('./checkpoints')
+    trainer.train(epoch=2, checkpoint_load_path='./checkpoints/model_epoch_1_save_9.pt',
+                  checkpoint=True)

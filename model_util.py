67ce40a5e2a7df74f8b759fd5d0b86d0e6a49198
wangzhesun <wangzhe.sun@outlook.com>
2022-06-07 20:49:12 -0500

    Update model_util.py

--- model_util.py
+++ model_util.py
@@ -6,11 +6,20 @@ def adjust_lr(lr, epoch, step):
     return lr * (0.5 ** (epoch // step))
 
 
-def save_checkpoint(model, epoch, path):
-    model_out_path = path + "/model_epoch_{}.pt".format(epoch)
-    if not os.path.exists(path):
-        os.makedirs(path)
+def save_checkpoint(model, epoch=-1, mark=-1, path=None, final=False):
+    if final:
+        model_out_path = path + "/final_model_weights.pt"
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-    torch.save(model.state_dict(), model_out_path)
+        torch.save(model.state_dict(), model_out_path)
 
-    print("Checkpoint saved to {}".format(model_out_path))
+        print("Final checkpoint saved to {}".format(model_out_path))
+    else:
+        model_out_path = path + "/model_epoch_{}_save_{}.pt".format(epoch, mark)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        torch.save(model.state_dict(), model_out_path)
+
+        print("Checkpoint saved to {}".format(model_out_path))

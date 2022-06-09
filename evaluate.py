a551f5e73158db83bcb6444a700872e3cc8e5aa2
wangzhesun <wangzhe.sun@outlook.com>
2022-06-08 21:02:47 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -10,12 +10,19 @@ def evaluate_img(model, input_path, cuda=False):
         input_image = input_image.cuda()
 
     output_image = model(input_image)
-    return output_image
+    return input_image, output_image
 
 
 if __name__ == '__main__':
+    print('initializing model ...')
     sr_model = model_util.create_model(scale=4)
-    sr_model = model_util.load_checkpoint(sr_model, './checkpoints/model_epoch_7_save_1.pt')
 
-    output = evaluate_img(sr_model, './data/DIV2K/DIV2K_test_LR_unknown/X4/0904x4.png')
-    image_util.show_tensor_img(output, 'output image')
+    sr_model, _, _ = model_util.load_checkpoint(sr_model, './checkpoints/model_epoch_7_save_1.pt')
+
+    print('enhancing image ...')
+    input_img, output_img = evaluate_img(sr_model,
+                                         './data/DIV2K/DIV2K_test_LR_unknown/X4/0904x4.png')
+
+    print('image displayed')
+    image_util.show_tensor_img(input_img.detach(), 'input image')
+    image_util.show_tensor_img(output_img.detach(), 'output image')

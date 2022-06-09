37f7521160d4cc28f5fb4e6a0610732f227e9b2f
wangzhesun <wangzhe.sun@outlook.com>
2022-06-08 21:49:14 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,20 @@
-from utils import dataset
-import torchvision.transforms as transforms
-from utils import model_util as util
+from utils.dataset import Dataset
+from torchvision.transforms import ToTensor
+from utils.model_util import create_model, train_model
 
 
 if __name__ == '__main__':
     # load dataset
     print('loading dataset ...')
-    train_data = dataset.Dataset(train_root='./data/DIV2K_aug/DIV2K_train_LR_bicubic_X4_aug',
-                                 target_root='./data/DIV2K_aug/DIV2K_train_HR_X4_aug',
-                                 transform=transforms.ToTensor())
+    train_data = Dataset(train_root='./data/DIV2K_aug/DIV2K_train_LR_bicubic_X4_aug',
+                         target_root='./data/DIV2K_aug/DIV2K_train_HR_X4_aug',
+                         transform=ToTensor())
 
     # create the model
     print('initializing model ...')
-    sr_model = util.create_model(scale=4)
+    sr_model = create_model(scale=4)
 
     # train the model
-    util.train_model(model=sr_model, train_dataset=train_data, epoch=100,
-                     checkpoint_save_path='checkpoints', checkpoint=False,
-                     checkpoint_load_path=None, cuda=False)
+    train_model(model=sr_model, train_dataset=train_data, epoch=100,
+                checkpoint_save_path='checkpoints', checkpoint=False, checkpoint_load_path=None,
+                cuda=False)

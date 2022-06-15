59e8a2da1b1c9419ca437941f52f6d9e10e6ff1e
wangzhesun <wangzhe.sun@outlook.com>
2022-06-15 00:04:58 -0500

    Update train.py

--- train.py
+++ train.py
@@ -6,11 +6,11 @@ from utils.model_util import train_model
 if __name__ == '__main__':
     # load dataset
     print('loading dataset ...')
-    train_data = Dataset(train_root='./data/DIV2K_aug/DIV2K_train_LR_bicubic_X4_aug',
-                         target_root='./data/DIV2K_aug/DIV2K_train_HR_X4_aug',
+    train_data = Dataset(train_root='train_dataset_path',
+                         target_root='target_dataset_path',
                          transform=ToTensor())
 
     # train the model
-    train_model(scale=4, train_dataset=train_data, epoch=100,
+    train_model(scale=4, train_dataset=train_data, epoch=100, lr=1e-4, batch_size=16,
                 checkpoint_save_path='checkpoints', checkpoint=False, checkpoint_load_path=None,
                 cuda=False)

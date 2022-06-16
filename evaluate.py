96d640d6af4761806370a56c2d7a6c773c9c52a3
wangzhesun <wangzhe.sun@outlook.com>
2022-06-15 20:02:34 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,5 +3,5 @@ from utils.model_util import enhance
 
 if __name__ == '__main__':
     # make sure you change the image_path to the path of the input image
-    enhance(scale=4, image_path='input_image_path/input_image_name', pre_train=True, display=True,
+    enhance(scale=4, image_path='input_image_path', pre_train=True, display=True,
             save=True, output_path='results', cuda=False)

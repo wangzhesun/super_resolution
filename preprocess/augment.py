bca3eb04b78871483bd9451c62f5ac5782ef0b33
wangzhesun <wangzhe.sun@outlook.com>
2022-06-15 19:37:25 -0500

    Update augment.py

--- preprocess/augment.py
+++ preprocess/augment.py
@@ -1,6 +1,10 @@
 from utils import image_util as util
 
 if __name__ == '__main__':
+    # make sure you change the train_root to the path of the raw training data directory,
+    # the target_root to the path of the raw target directory, the train_output_path to the desired
+    # output path of augmented training data, and the target_output_path to the desired output path
+    # of augmented target data
     util.augment_dir(train_root='raw_train_dataset_path',
                      target_root='raw_target_dataset_path',
                      train_output_path='augmented_train_dataset_output_path',
